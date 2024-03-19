ed6b3da862a000abddb4ff209a298f52310c8a0c
ArmandoReyes105 <arrey105@outlook.com>
2024-03-19 17:01:33 -0600

    Creacion del repositorio

---
+++ HealthDivineSysClient/Helpers/NavigationManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls; 
+
+namespace HealthDivineSysClient.Helpers
+{
+    public class NavigationManager
+    {
+
+        private static NavigationManager _instance;
+        private Frame _mainFrame;
+
+        private NavigationManager() { }
+
+        public static NavigationManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new NavigationManager();
+                }
+                return _instance;
+            }
+        }
+
+        public void Initialize(Frame mainFrame)
+        {
+            _mainFrame = mainFrame;
+        }
+
+        public void NavigateTo(Page page)
+        {
+            if (_mainFrame != null)
+            {
+                _mainFrame.Navigate(page);
+            }
+        }
+
+    }
+}
---
+++ HealthDivineSysClient/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using HealthDivineSysClient.Helpers;
+using HealthDivineSysClient.View; 
+using System.Windows;
+
+namespace HealthDivineSysClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NavigationManager.Instance.Initialize(Frame_Main);
+            NavigationManager.Instance.NavigateTo(new AddPatient1()); 
+        }
+    }
+}
